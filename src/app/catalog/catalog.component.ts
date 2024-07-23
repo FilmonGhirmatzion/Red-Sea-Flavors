ac246bf4ceded82a22125b988b0abde7473fbb1f
Ghirmatzion, Filmon <ghirmf1@nationwide.com>
2024-07-22 19:18:10 -0500

    binding and function attribute

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent implements OnInit {
+export class CatalogComponent {
+      product: IProduct;
 
-  constructor() { }
+      constructor(){
+        this.product={
+          id: 1,
+          description:
+            "Qlql is made of Meet it healthy, made with organic ingredients.",
+          name: "Qlql",
+          imageName: "qlql.jpg",
+          category: "Meat Dishes",
+          price: 12.5,
+          discount: 0.25,
+        };
+      }
 
-  ngOnInit(): void {
-  }
+    getImageUrl(product: IProduct){
+      return '/assets/images/food-items/' + product.imageName;
+
+    }
 
 }
